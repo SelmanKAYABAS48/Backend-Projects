b0b7ea1095b43a782ad715e3e36c239765c1ed82
SelmanKAYABAS48 <selmankayabas48@gmail.com>
2024-08-14 20:56:25 +0300

    up

--- 27-StockApi/src/controllers/auth.js
+++ 27-StockApi/src/controllers/auth.js
@@ -76,7 +76,6 @@ module.exports = {
     },
 
     refresh: async (req, res) => {
-
         /*
             #swagger.tags = ['Authentication']
             #swagger.summary = 'JWT: Refresh'
@@ -92,63 +91,90 @@ module.exports = {
             }
         */
 
-            const refreshToken = req.body?.bearer?.refreshToken
+        const refreshToken = req.body?.bearer?.refreshToken
 
-            if (refreshToken) {
-    
-                jwt.verify(refreshToken, process.env.REFRESH_KEY, async function (err, userData) {
-    
-                    if (err) {
-    
-                        res.errorStatusCode = 401
-                        throw err
-                    } else {
-    
-                        const { _id, password } = userData
-    
-                        if (_id && password) {
-    
-                            const user = await User.findOne({ _id })
-    
-                            if (user && user.password == password) {
-    
-                                if (user.isActive) {
-    
-                                    // JWT:
-                                    const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_KEY, { expiresIn: '30m' })
-    
-                                    res.send({
-                                        error: false,
-                                        bearer: { accessToken }
-                                    })
-    
-                                } else {
-    
-                                    res.errorStatusCode = 401
-                                    throw new Error('This account is not active.')
-                                }
+        if (refreshToken) {
+
+            jwt.verify(refreshToken, process.env.REFRESH_KEY, async function (err, userData) {
+
+                if (err) {
+
+                    res.errorStatusCode = 401
+                    throw err
+                } else {
+
+                    const { _id, password } = userData
+
+                    if (_id && password) {
+
+                        const user = await User.findOne({ _id })
+
+                        if (user && user.password == password) {
+
+                            if (user.isActive) {
+
+                                // JWT:
+                                const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_KEY, { expiresIn: '30m' })
+
+                                res.send({
+                                    error: false,
+                                    bearer: { accessToken }
+                                })
+
                             } else {
-    
+
                                 res.errorStatusCode = 401
-                                throw new Error('Wrong id or password.')
+                                throw new Error('This account is not active.')
                             }
                         } else {
-    
-                            res.errorStatusCode = 401
-                            throw new Error('Please enter id and password.')
-                        }
-                    }
-                })
-    
-            } else {
-                res.errorStatusCode = 401
-                throw new Error('Please enter token.refresh')
-            }
 
+                            res.errorStatusCode = 401
+                            throw new Error('Wrong id or password.')
+                        }
+                    } else {
+
+                        res.errorStatusCode = 401
+                        throw new Error('Please enter id and password.')
+                    }
+                }
+            })
+
+        } else {
+            res.errorStatusCode = 401
+            throw new Error('Please enter token.refresh')
+        }
     },
 
     logout: async (req, res) => {
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Token: Logout"
+            #swagger.description = 'Delete token-key.'
+        */
 
+        const auth = req.headers?.authorization || null // Token ...tokenKey... // Bearer ...accessToken...
+        const tokenKey = auth ? auth.split(' ') : null // ['Token', '...tokenKey...'] // ['Bearer', '...accessToken...']
+
+        let message = null, result = {}
+
+        if (tokenKey) {
+
+            if (tokenKey[0] == 'Token') { // SimpleToken
+
+                result = await Token.deleteOne({ token: tokenKey[1] })
+                message = 'Token deleted. Logout was OK.'
+
+            } else { // JWT
+
+                message = 'No need any process for logout. You must delete JWT tokens.'
+            }
+        }
+
+        res.send({
+            error: false,
+            message,
+            result
+        })
     },
 
 }
